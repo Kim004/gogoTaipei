f8a9c3c202cfcb4932287ca30268889f9672bab2
TaylorLu <aza110390@gmail.com>
2019-12-04 03:11:07 +0800

    套模板

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,25 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('my_view.index');
+})->name('home');
+
+Route::get('about', function () {
+    return view('my_view.about');
+})->name('about');
+
+Route::get('listings', function () {
+    return view('my_view.listings');
+})->name('listings');
+
+Route::get('contact', function () {
+    return view('my_view.contact');
+})->name('contact');
+
+Route::get('blog', function () {
+    return view('my_view.blog');
+})->name('blog');
+
 
 Auth::routes();
 
